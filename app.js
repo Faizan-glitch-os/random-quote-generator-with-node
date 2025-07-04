a9a6e8b77836e522ac65a95e1df87037dcdffff7
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-07-04 15:42:06 +0500

    api done, it will server random quote on every call

---
+++ app.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("dev"));
+
+const quotes = JSON.parse(fs.readFileSync("./quotes.json", "utf-8"));
+
+app.get("/api/v1/random-quote", (req, res) => {
+  const quotesLength = quotes.quotes.length;
+  const randomIndex = Math.floor(Math.random() * quotesLength);
+  console.log(randomIndex);
+
+  res.status(200).json({
+    status: "success",
+    author: quotes.quotes[randomIndex].author,
+    quote: quotes.quotes[randomIndex].quote,
+  });
+});
+
+app.all("*splate", (req, res) => {
+  res
+    .status(400)
+    .json({ status: "fail", message: "no such route defined on this server" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
